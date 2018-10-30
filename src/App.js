a6c444d71a17bc3f47da3fa251234e15c219b016
Greg <g.maceachern@queensu.ca>
2018-10-30 17:11:31 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+const kijiji = require("kijiji-scraper");
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchTerm: '',
+            query: ''
+
+        }
+    }
+    updateText = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        });
+    }
+    scrape = () => {
+        this.setState({
+            query: this.state.searchTerm
+        });
+
+        let options = {
+            minResults: 40
+        };
+         
+        let params = {
+            locationId: 1700185,  // Same as kijiji.locations.ONTARIO.OTTAWA_GATINEAU_AREA.OTTAWA
+            categoryId: 27,  // Same as kijiji.categories.CARS_AND_VEHICLES
+            sortByName: "priceAsc"  // Show the cheapest listings first
+        };
+         
+        // Scrape using returned promise
+        kijiji.search(params, options).then(function(ads) {
+            // Use the ads array
+            for (let i = 0; i < ads.length; ++i) {
+                console.log(ads[i].title);
+            }
+        }).catch(console.error);
+         
+        // Scrape using optional callback parameter
+        function callback(err, ads) {
+            if (!err) {
+                // Use the ads array
+                for (let i = 0; i < ads.length; ++i) {
+                    console.log(ads[i].title);
+                }
+            }
+        }
+        kijiji.search(params, options, callback);
+    }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type='text' onChange={this.updateText} placeholder= 'Enter the product you wish to search' />
+        <input type='button' onClick={this.scrape} value='Search'/>
+        <h1>Average price for a used {this.state.query}</h1>
       </div>
     );
   }
 }
 
 export default App;
+
